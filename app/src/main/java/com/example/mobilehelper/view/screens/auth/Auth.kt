a2e9aa2b9d56f0368aef116200a5969eb4906482
klmnvan <126891264+klmnvan@users.noreply.github.com>
2024-12-18 00:37:56 +0300

    aaaaaaaa

--- app/src/main/java/com/example/mobilehelper/view/screens/auth/Auth.kt
+++ app/src/main/java/com/example/mobilehelper/view/screens/auth/Auth.kt
@@ -1,6 +1,7 @@
 package com.example.mobilehelper.view.screens.auth
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,13 +11,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -29,24 +34,69 @@ import androidx.navigation.NavHostController
 fun Auth(navHostController: NavHostController) {
 
     val context = LocalContext.current
-    //Такая конструкция позволит при перекомпоновке данных не пересоздавать объект viewModel
     val vm = viewModel { AuthViewModel() }
 
-    Column(modifier = Modifier.fillMaxSize().padding(horizontal = 20.dp),
+    Column(modifier = Modifier.fillMaxSize().padding(horizontal = 20.dp).verticalScroll(
+        rememberScrollState()
+    ),
         verticalArrangement = Arrangement.Center) {
+
+
+        val state = vm.state.collectAsState()
+
+        LaunchedEffect(Unit) {
+            vm.init()
+        }
+
+        LaunchedEffect(state.value.selectedCategory) {
+            vm.filter()
+        }
+
+        OutlinedTextField(
+            value = state.value.search,
+            onValueChange = {
+                vm.stateValue = state.value.copy(search = it)
+                vm.filter()
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
+
+        state.value.categories.forEach {
+            Text(it.category, modifier = Modifier.background(if (state.value.selectedCategory == it) Color.Red else Color.Gray).clickable {
+                vm.stateValue = state.value.copy(selectedCategory = it)
+            }.padding(end = 15.dp) )
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        state.value.filmPreview.forEach {
+            Text(it.name)
+        }
+
+
+
+        /*
+        val configuration = LocalConfiguration.current
+        val screenHeight = configuration.screenHeightDp.dp
+        val screenWidth = configuration.screenWidthDp
+        Row(modifier = Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.Center) {
+            Box(modifier = Modifier.height(10.dp).width(screenHeight * 0.05f).background(Color.Gray, RoundedCornerShape(5.dp)))
+            Spacer(modifier = Modifier.width(10.dp))
+            Box(modifier = Modifier.height(10.dp).width(screenHeight * 0.05f).background(Color.Gray, RoundedCornerShape(5.dp)))
+            Spacer(modifier = Modifier.width(10.dp))
+            Box(modifier = Modifier.height(10.dp).width(screenHeight * 0.08f).background(Color.Black, RoundedCornerShape(5.dp)))
+        }*//*
+        Spacer(modifier = Modifier.height(15.dp))
+        Spacer(modifier = Modifier.weight(1f))
         TextField(vm.userEmail, { vm.userEmail = it }, "Email")
-        TextField(vm.surname, { vm.surname = it }, "Фамилия")
+        TextField(vm.surname, { vm.surname = it }, "Фамилия")*//*
         Button( onClick = { vm.signIn(context) } ) { Text("Авторизация") }
         Button( onClick = { vm.updateProfile(context) } ) { Text("Изменить") }
         Button( onClick = { vm.getRole(context) } ) { Text("Получить роль") }
-        Text(vm.listUser.toString())
-        if(vm.role == "1") {
-            Text("Функционал для роли 1")
-        }
-        if(vm.role == "2") {
-            Text("Функционал для роли 2")
-        }
-        LazyColumn(modifier = Modifier.fillMaxWidth()) {
+        Button( onClick = { vm.getRole(context) } ) { Text("Получить роль") }
+        Button( onClick = { vm.getRole(context) } ) { Text("Получить роль") }
+        Button( onClick = { vm.insert() } ) { Text("Insert") }*/
+        /*LazyColumn(modifier = Modifier.fillMaxWidth()) {
             items(vm.listUser, key = { user -> user.id } ) { user ->
                 Column(modifier = Modifier.fillMaxWidth().background(Color.LightGray, RoundedCornerShape(15.dp)).padding(15.dp)) {
                     Text(user.name)
@@ -56,7 +106,11 @@ fun Auth(navHostController: NavHostController) {
                 }
                 Spacer(modifier = Modifier.height(10.dp))
             }
-        }
+        }*//*
+        Spacer(modifier = Modifier.height(15.dp))
+        Spacer(modifier = Modifier.weight(1f))
+        Button( onClick = { vm.insert() } ) { Text("Insert") }
+        Spacer(modifier = Modifier.height(15.dp))*/
     }
 
 }
--- app/src/main/java/com/example/mobilehelper/view/screens/auth/AuthViewModel.kt
+++ app/src/main/java/com/example/mobilehelper/view/screens/auth/AuthViewModel.kt
@@ -12,22 +12,117 @@ import com.example.mobilehelper.domain.Constants
 import io.github.jan.supabase.gotrue.auth
 import io.github.jan.supabase.gotrue.providers.builtin.Email
 import io.github.jan.supabase.postgrest.from
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+
+@Serializable
+data class User(
+    var id: String = "",
+    var name: String = "",
+    var surname: String = "",
+    var patronymic: String = "",
+    @SerialName("id_role")
+    var idRole: Int = 1
+)
+
+data class State(
+    val search: String = "",
+    val selectedCategory: Category = Category(0, ""),
+    val filmPreview: MutableList<Film> = mutableListOf(),
+    val categories: MutableList<Category> = mutableListOf(),
+    val categoryFilms: MutableList<FilmCategory> = mutableListOf(),
+    val films: MutableList<Film> = mutableListOf(),
+)
+
+data class Film(
+    val id: Int,
+    val name: String
+)
+
+data class Category(
+    val id: Int,
+    val category: String
+)
+
+data class FilmCategory(
+    val idFilm: Int,
+    val idCategory: Int
+)
 
 class AuthViewModel: ViewModel() {
 
     var userEmail by mutableStateOf("")
     var surname by mutableStateOf("")
+
+
+    private val _state = MutableStateFlow(State())
+    val state: StateFlow<State> = _state.asStateFlow()
+
+    var stateValue: State
+        get() = _state.value
+        set(value) {
+            _state.value = value
+        }
+
+    fun init() {
+        stateValue = stateValue.copy(categories = mutableListOf(Category(1, "триллер"), Category(2,"хоррор")))
+        stateValue = stateValue.copy(films = mutableListOf(Film(5, "film2"), Film(6,"film1")))
+        stateValue = stateValue.copy(categoryFilms = mutableListOf(FilmCategory(5, 1), FilmCategory(6,2), FilmCategory(5,2)))
+        if(stateValue.categories.isNotEmpty()) {
+            stateValue = stateValue.copy(selectedCategory = stateValue.categories.first())
+            filter()
+        }
+    }
+
+    fun filter() {
+        //получаем idшники фильмов выбранной категории
+        val sortedFilms = stateValue.categoryFilms.filter { it.idCategory == stateValue.selectedCategory.id }
+        stateValue.filmPreview.clear()
+        //перебираем список фильмов, и в список, который выводим, добавляем нужные фильмы
+        val filmPreview = mutableListOf<Film>()
+        sortedFilms.forEach { filmCategory ->
+            filmPreview.add(stateValue.films.first { it.id == filmCategory.idFilm })
+        }
+        stateValue = stateValue.copy(filmPreview = filmPreview)
+        //stateValue = stateValue.copy(filmPreview = stateValue.filmPreview.filter { it.name.contains(stateValue.search, ignoreCase = true) }.toMutableList())
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     private var userPassword by mutableStateOf("123456")
     var listUser by mutableStateOf(listOf<User>())
     var role by mutableStateOf("")
 
+    /*fun insert() {
+        viewModelScope.launch {
+            try {
+                Constants.supabase.from("test").insert(
+                    value = mapOf(Pair("name", "chto-to"), Pair("col", "chto-to"))
+                )
+            } catch (e: Exception) {
+                Log.d("insert", e.message.toString())
+            }
+        }
+    }
+
     fun signIn(context: Context) {
         viewModelScope.launch {
             try {
-                Constants.supabase.auth.signInWith(Email){
+                Constants.supabase.auth.signInWith(Email) {
                     this.email = userEmail
                     this.password = userPassword
                 }
@@ -84,18 +179,9 @@ class AuthViewModel: ViewModel() {
     }
 
     suspend fun getUsers() {
-        listUser = Constants.supabase.from("users").select().decodeList<User>()
+        listUser = Constants.supabase.from("users")
+            .select().decodeSingle()
         Log.e("user", listUser.toString())
-    }
+    }*/
 
 }
-
-@Serializable
-data class User(
-    var id: String = "",
-    var name: String = "",
-    var surname: String = "",
-    var patronymic: String = "",
-    @SerialName("id_role")
-    var idRole: Int = 1
-)
